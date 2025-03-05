659fe74a3fdcbfd0d23df18cf32bc6cec5377029
reason-jinny <reason.jinny@gmail.com>
2025-03-05 20:34:30 +0900

    init: Spring Boot + Vue 프로젝트 초기 설정

---
+++ backend/src/main/java/com/kt/jigaknono/JigaknonoApplication.java
@@ -0,0 +1,13 @@
+package com.kt.jigaknono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigaknonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigaknonoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kt/jigaknono/JigaknonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.jigaknono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigaknonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

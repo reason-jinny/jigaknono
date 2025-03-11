b1039b63e10018bd0d0e286ccca17be96266798c
reason-jinny <reason.jinny@gmail.com>
2025-03-11 15:04:55 +0900

    feat: 관리자 계정 페이지 라우트 추가 - AdminAccountView 컴포넌트 라우터에 등록 - 관리자 인증 가드 적용하여 로그인 상태에서만 접근 가능

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AdminView from '../views/AdminView.vue'
 import AdminLoginView from '../views/AdminLoginView.vue'
+import AdminAccountView from '../views/AdminAccountView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,18 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: AdminView,
+      beforeEnter: (to, from, next) => {
+        if (!localStorage.getItem('adminAuthenticated')) {
+          next('/admin/login');
+        } else {
+          next();
+        }
+      }
+    },
+    {
+      path: '/admin/account',
+      name: 'adminAccount',
+      component: AdminAccountView,
       beforeEnter: (to, from, next) => {
         if (!localStorage.getItem('adminAuthenticated')) {
           next('/admin/login');
